262be91f97f87199c165b2ef6c5cda3381a1f15e
unknown <suyomail@naver.com>
2022-11-17 19:18:58 +0900

    feat(InputView): 다리길이, 이동 방향, 다시 시도 여부 입력 받기

---
+++ src/main/java/bridge/Messages.java
@@ -0,0 +1,7 @@
+package bridge;
+
+public class Messages {
+    public static final String BRIDGE_SIZE_MESSAGE = "\n다리의 길이를 입력해주세요.";
+    public static final String MOVING_DIRECTION_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RETRY_CHOICE_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+}
